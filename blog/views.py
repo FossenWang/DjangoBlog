7ae5388764bfd595ba86772e0a85b202d4e33f2a
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-11-10 21:36:51 +0800

    markdown

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+import markdown
 
 # Create your views here.
 
@@ -9,4 +10,5 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.content = markdown.markdown(post.content, extensions=['markdown.extensions.extra','markdown.extensions.codehilite','markdown.extensions.toc',])
     return render(request, 'blog/detail.html', context={'post': post})
