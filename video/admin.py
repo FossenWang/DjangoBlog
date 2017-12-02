5ad68926a0ab16b31e3097f5b007d9a7b94af499
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-12-02 21:26:15 +0800

    index view 2

--- video/admin.py
+++ video/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Category, Tag, Video
 
-admin.site.register([Category, Tag, Video])
+class VideoInline(admin.TabularInline):
+    model = Video
+    fields = ['title', 'video', 'pub_date', 'author']
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name',]
+    inlines = [VideoInline]
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'pub_date')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Tag)
--- video/views.py
+++ video/views.py
@@ -2,89 +2,24 @@
 视频推送相关的视图
 '''
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
 from django.http import HttpResponseRedirect
+from django.db.models.aggregates import Count
 
 from .models import Video, Category, Tag
 
 
-class IndexView(ListView):
+class IndexView(TemplateView):
     '首页视图'
-    model = Video
     template_name = 'video/index.html'
-    context_object_name = 'recent_video_list'
-    #paginate_by = 10
 
     def get_context_data(self, **kwargs):
         '''
-        覆盖该方法
+        获取首页展示的内容
         '''
         context = super().get_context_data(**kwargs)
-        #paginator = context.get('paginator')
-        #page = context.get('page_obj')
-        #is_paginated = context.get('is_paginated')
-        #pagination_data = self.pagination_data(paginator, page, is_paginated)
-        #context.update(pagination_data)
-        context['recent_video_list'] = Video.objects.all()[:4]
+        context['latest_video'] = Video.objects.all()[0]
+        context['recent_videos'] = Video.objects.all()[1:5]
+        cates = Category.objects.annotate(counts=Count('video')).filter(counts__gt=0)
+        context['video_categories'] = [(Video.objects.filter(category__name=cate.name)[:4], cate) for cate in cates]
         return context
-
-    '''
-    def pagination_data(self, paginator, page, is_paginated):
-        if not is_paginated:
-            return {}
-
-        left, right= [], []
-        left_has_more = False
-        right_has_more = False
-        first = False
-        last = False
-        page_number = page.number
-        total_pages = paginator.num_pages
-        page_range = paginator.page_range
-
-        if page_number == 1:
-            right = page_range[page_number:page_number + 2]
-            if right[-1] < total_pages - 1:
-                right_has_more = True
-            if right[-1] < total_pages:
-                last = True
-        elif page_number == total_pages:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-            if left[0] > 2:
-                left_has_more = True
-            if left[0] > 1:
-                first = True
-        else:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-            right = page_range[page_number:page_number + 2]
-            if right[-1] < total_pages - 1:
-                right_has_more = True
-            if right[-1] < total_pages:
-                last = True
-            if left[0] > 2:
-                left_has_more = True
-            if left[0] > 1:
-                first = True
-
-        data = {
-            'left': left,
-            'right': right,
-            'left_has_more': left_has_more,
-            'right_has_more': right_has_more,
-            'first': first,
-            'last': last,
-        }
-
-        return data
-        
-
-    def upload_image(request):
-    if request.method == 'POST':
-        form = ModelFormWithFileField(request.POST, request.FILES)
-        if form.is_valid():
-            # file is saved
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = ModelFormWithFileField()
-    return render(request, 'upload.html', {'form': form})'''
