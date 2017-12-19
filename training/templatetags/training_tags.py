174a50aa53aedebc9c8b14fad0edae7f2f8d027d
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-12-19 21:32:15 +0800

    training app 6

--- training/templatetags/training_tags.py
+++ training/templatetags/training_tags.py
@@ -1,6 +1,6 @@
 '模板标签'
 from django import template
-from ..models import Program, ProgramType
+from ..models import Program, ProgramType, Exercise, ExerciseType
 from django.db.models.aggregates import Count
 
 register = template.Library()
@@ -14,3 +14,20 @@ def get_program_types():
 def get_program_count():
     '训练方案总数'
     return Program.objects.all().count()
+
+@register.simple_tag
+def get_exercise_types():
+    '动作分类'
+    sorts = ExerciseType.objects.annotate(counts=Count('exercise')).filter(counts__gt=0)
+    etypes = {}
+    for sort in sorts:
+        if sort.etype not in etypes.keys():
+            etype_sorts = sorts.filter(etype=sort.etype)
+            ecounts = sum([s.counts for s in etype_sorts])
+            etypes[sort.etype] = [etype_sorts, str(sort.number)[0], ecounts]
+    return etypes
+
+@register.simple_tag
+def get_exercise_count():
+    '动作总数'
+    return Exercise.objects.all().count()
--- training/views.py
+++ training/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
-from .models import Program, ProgramType
+from .models import Program, ProgramType, Exercise, ExerciseType
 
 class ProgramListView(ListView):
     '训练方案列表'
@@ -86,3 +86,79 @@ class ProgramDetailView(DetailView):
     model = Program
     template_name = 'training/program_detail.html'
     context_object_name = 'program'
+
+class ExerciseListView(ListView):
+    '动作库'
+    model = Exercise
+    template_name = 'training/exercise_list.html'
+    context_object_name = 'exercise_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        if self.kwargs.get('number') == '0':
+            return super().get_queryset()
+        else:
+            return super().get_queryset().filter(sort__number__startswith=self.kwargs.get('number'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+
+        context['number'] = self.kwargs.get('number')
+
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        '分页数据'
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
